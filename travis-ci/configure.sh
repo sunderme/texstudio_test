514b753526689f87fba102975527ed90532a2510
sunderme@gmx.de <sunderme@gmx.de>
2017-09-01 21:37:26 +0200

    fix configure

--- travis-ci/configure.sh
+++ travis-ci/configure.sh
@@ -10,14 +10,11 @@ cd "${TRAVIS_BUILD_DIR}"
 print_headline "Configuring for building for ${QT} on ${TRAVIS_OS_NAME}"
 
 if [ "${QT}" = "qt5" ]; then
-	export DISPLAY=:99.0
-	sh -e /etc/init.d/xvfb start
-	sleep 3 # give xvfb some time to start
 	qmake texstudio.pro CONFIG+=debug
 elif [ $QT = qt5NoPoppler ]; then
-	qmake texstudio.pro NO_POPPLER_PREVIEW=1
+	qmake texstudio.pro CONFIG+=debug NO_POPPLER_PREVIEW=1
 elif [ $QT = qt4 ]; then
-	qmake texstudio.pro 
+	qmake texstudio.pro CONFIG+=debug
 elif [ "${TRAVIS_OS_NAME}" = "osx" ]; then
 	qmake texstudio.pro CONFIG-=debug
 fi
