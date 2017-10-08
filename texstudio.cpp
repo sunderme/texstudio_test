a9e094c04f009e75f5e597b31abcb36069f113f0
Tim Hoffmann <thoffmann@texstudio.org>
2017-10-08 20:20:56 +0200

    group all close element options on shortcuts page

--- texstudio.cpp
+++ texstudio.cpp
@@ -1135,7 +1135,7 @@ void Texstudio::setupMenus()
 	newManagedAction(menu, "enlargePDF", tr("Show embedded PDF large"), SLOT(enlargeEmbeddedPDFViewer()));
 	newManagedAction(menu, "shrinkPDF", tr("Show embedded PDF small"), SLOT(shrinkEmbeddedPDFViewer()));
 
-	newManagedAction(menu, "closesomething", tr("Close Something"), SLOT(viewCloseSomething()), Qt::Key_Escape);
+	newManagedAction(menu, "closeelement", tr("Close Element"), SLOT(viewCloseElement()), Qt::Key_Escape);
 
 	menu->addSeparator();
 	submenu = newManagedMenu(menu, "collapse", tr("Collapse"));
@@ -6894,7 +6894,7 @@ void Texstudio::focusViewer()
 #endif
 }
 
-void Texstudio::viewCloseSomething()
+void Texstudio::viewCloseElement()
 {
 	if (fileSelector) {
 		fileSelector.data()->deleteLater();
@@ -6919,7 +6919,7 @@ void Texstudio::viewCloseSomething()
 	if (qobject_cast<PDFDocument *>(w)) {
 		PDFDocument *focusedPdf = qobject_cast<PDFDocument *>(w);
 		if (focusedPdf->embeddedMode) {
-			bool pdfClosed = focusedPdf->closeSomething();
+			bool pdfClosed = focusedPdf->closeElement();
 			if (pdfClosed) {
 				focusEditor();
 			} else {
@@ -6935,7 +6935,7 @@ void Texstudio::viewCloseSomething()
 		textAnalysisDlg->close();
 		return;
 	}
-	if (currentEditorView() && currentEditorView()->closeSomething())
+	if (currentEditorView() && currentEditorView()->closeElement())
 		return;
 	if (outputView->isVisible() && configManager.useEscForClosingLog) {
 		outputView->hide();
@@ -6948,7 +6948,7 @@ void Texstudio::viewCloseSomething()
 			return;
 		}
 #endif
-	if (windowState() == Qt::WindowFullScreen && !configManager.disableEscForClosingFullscreen) {
+	if (windowState() == Qt::WindowFullScreen && configManager.useEscForClosingFullscreen) {
 		stateFullScreen = saveState(1);
 		setWindowState(Qt::WindowNoState);
 		restoreState(windowstate, 0);
